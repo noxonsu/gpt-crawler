ab300b024271109af2668742bec3e3aabdd85d51
Noxon <2914674+noxonsu@users.noreply.github.com>
2023-12-18 19:38:15 +0000

    Update config.ts and package.json

--- config.ts
+++ config.ts
@@ -1,8 +1,13 @@
 import { Config } from "./src/config";
+import dotenv from "dotenv";
+import { Config } from "./src/config";
+
+
+dotenv.config();
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
+  url: process.env.URL || "https://drivingschool.wpmix.net/",
+  match: process.env.MATCH || "https://drivingschool.wpmix.net/",
   maxPagesToCrawl: 50,
   outputFileName: "output.json",
 };
