bcbcbc8c742dc63b7c6d4334884ade33d566e11b
Noxon <2914674+noxonsu@users.noreply.github.com>
2023-12-18 19:55:40 +0000

    Update output file name in config.ts

--- config.ts
+++ config.ts
@@ -1,13 +1,10 @@
+
 import { Config } from "./src/config";
-        import dotenv from "dotenv";
-        import { Config } from "./src/config";
-        
-        
-        dotenv.config();
-        
-        export const defaultConfig: Config = {
-          url: "${{ github.event.inputs.URL }}",
-          match: "${{ github.event.inputs.URL }}/${{ github.event.inputs.MATCH }}",
-          maxPagesToCrawl: 50,
-          outputFileName: "output.json",
-        };
+
+export const defaultConfig: Config = {
+  url: "https://drivingschool.wpmix.net/",
+  match: "https://drivingschool.wpmix.net//**",
+  maxPagesToCrawl: 50,
+  outputFileName: "output.json",
+  resourceExclusions: ["jpg","png"]
+};
